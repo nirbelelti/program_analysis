9e800fa057ca88dab6e051151fb87270b5e8cb55
nirbelelti <nirbelelti@gmail.com>
2023-09-17 21:10:21 +0200

    Work in progress

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import json
+
 from graphviz import Digraph
 
 json_data = open('car.json')
+
+
 def create_uml_from_json(json_data):
 
 
     data = json.loads(json_data)
-    print("data"+ str(data))
+    print("data" + str(data))
 
     # Create a new Digraph
     dot = Digraph('UML Diagram', format='png')
@@ -18,49 +21,22 @@ def create_uml_from_json(json_data):
 
         # Add associations for superclasses
         for superclass in class_info.get('super', []):
-            superclass_name = superclass[0]
+            superclass_name = superclass['name']
             dot.edge(class_name, superclass_name)
 
     # Set the output file name and render the diagram
     output_file = 'uml_diagram'
     dot.render(output_file, format='png')
 
+
 if __name__ == '__main__':
     # JSON data representing Java code metadata
-    json_data = '''
-    [
-        {
-            "name": "Main",
-            "access": ["public", "super"],
-            "typeparams": [],
-            "super": {
-                "name": "java/lang/Object",
-                "inner": null,
-                "args": [],
-                "annotations": []
-            },
-            "interfaces": [],
-            "fields": [],
-            "methods": [
-                {
-                    "name": "<init>",
-                    "access": ["public"],
-                    "typeparams": [],
-                    "params": [],
-                    "returns": {
-                        "type": null,
-                        "annotations": []
-                    },
-                    "annotations": [],
-                    "exceptions": [],
-                    "default": null
-                }
-            ],
-            "annotations": [],
-            "exceptions": [],
-            "default": null
-        }
-    ]
-    '''
+    file = open('car.json')
 
-    create_uml_from_json(json_data)
+    # returns JSON object as
+    # a dictionary
+
+    json_data = json.load(file)
+    json_data = json.dumps(json_data)
+
+    create_uml_from_json(str(json_data))
---
+++ main2.py
@@ -0,0 +1,46 @@
+import json
+
+from graphviz import Digraph
+
+
+def create_uml_from_json(data_dict):
+    # Create a new Digraph
+    dot = Digraph('UML Diagram', format='png')
+
+    # Iterate over each class in the dictionary
+    for class_name, data in data_dict.items():
+        # Build a label for the class with its attributes and methods
+        label = class_name + "|"
+
+        # Add attributes to the label
+        for attribute in data["attributes"]:
+            access_symbol = '+' if attribute["access"] == "public" else '-'
+            label += f"{access_symbol} {attribute['name']} : {attribute['type']}\\l"
+
+        # Add methods to the label
+        label += "|"
+        for method in data["methods"]:
+            access_symbol = '+' if method["access"] == "public" else '-'
+            param_list = ', '.join(
+                [f"{param['name']} : {param['type']}" for param in method.get("params", [])])
+            label += f"{access_symbol} {method['name']}({param_list}) : {method['returnType']}\\l"
+
+        dot.node(class_name, label, shape='record')
+
+        # Add relationships
+        if "relations" in data:
+            for relation in data["relations"]:
+                if relation["type"] == "inheritance":
+                    dot.edge(class_name, relation["target"])
+
+    # Set the output file name and render the diagram
+    output_file = 'uml_diagram'
+    dot.render(output_file, format='png')
+    print(f"Diagram saved as {output_file}.png")
+
+
+if __name__ == '__main__':
+    with open('car.json', 'r') as f:
+        data_dict = json.load(f)
+
+    create_uml_from_json(data_dict)

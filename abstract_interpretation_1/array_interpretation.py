8d41b66bb604bec07bd2ccd2457adbdf8edabc7b
nirbelelti <nirbelelti@gmail.com>
2023-10-01 19:20:19 +0200

    add Arrays.json file

---
+++ abstract_interpretation_1/array_interpretation.py
@@ -0,0 +1,81 @@
+class JavaStaticInterpreter:
+    def __init__(self):
+        self.array_length = None
+        self.index = None
+
+    def interpret(self, bytecode):
+        for instruction in bytecode:
+            offset = instruction['offset']
+            opr = instruction['opr']
+
+            if opr == 'newarray':
+                dim = instruction['dim']
+                if dim != 1:
+                    print(f'Offset {offset}: Multidimensional arrays are not supported.')
+                    return
+            elif opr == 'store' and instruction['type'] == 'ref':
+                index = instruction['index']
+                if index == 0:
+                    print(f'Offset {offset}: Stored reference to array.')
+                elif index == 1:
+                    print(f'Offset {offset}: Stored index variable.')
+
+            elif opr == 'load' and instruction['type'] == 'int':
+                print(f'Offset {offset}: Loaded integer variable.')
+
+            elif opr == 'arraylength':
+                if self.array_length is None or self.array_length < 1:
+                    print(f'Offset {offset}: Array length accessed.')
+                    self.array_length = True
+
+            elif opr == 'array_store':
+                print(f'Offset {offset}: Array element store operation.')
+
+            elif opr == 'load' and instruction['type'] == 'ref':
+                index = instruction['index']
+                if index == 0:
+                    print(f'Offset {offset}: Loaded array reference.')
+
+            elif opr == 'if' and instruction['condition'] == 'gt':
+                target = instruction['target']
+                print(f'Offset {offset}: Branching condition (greater than). Target offset: {target}')
+
+        # Check for potential IndexOutOfBoundsException
+        if self.array_length is not None and self.index is not None:
+            if self.index >= self.array_length:
+                print(f'Potential IndexOutOfBoundsException detected.')
+
+
+# Example bytecode for the array indexing
+bytecode = [
+
+    {
+        "offset": 0,
+        "opr": "load",
+        "type": "ref",
+        "index": 0
+    },
+    {
+        "offset": 1,
+        "opr": "push",
+        "value": {
+            "type": "integer",
+            "value": 0
+        }
+    },
+    {
+        "offset": 2,
+        "opr": "array_load",
+        "type": "int"
+    },
+    {
+        "offset": 3,
+        "opr": "return",
+        "type": "int"
+    }
+
+]
+
+# Create and run the static interpreter
+static_interpreter = JavaStaticInterpreter()
+static_interpreter.interpret(bytecode)
